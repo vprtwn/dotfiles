136e9c810422586f951287813f086731863bd48d
Ben Guo <ben@venmo.com>
2013-11-08 18:09:44 -0500

    initial install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+cp -v Resesif.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Resesif.dvtcolortheme
+cp -v DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+cp -v .vimrc ~/.vimrc
+cp -v .zshrc ~/.zshrc
+cp -v .gitconfig ~/.gitconfig
+
