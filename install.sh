a345e3703bad92c7dbfa553e096d70adafb69865
Ben Guo <ben@venmo.com>
2013-11-28 18:24:44 -0500

    updates install script

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
+# colors
 cp -v Resesif.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Resesif.dvtcolortheme
+
+# keybindings
 cp -v DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+
+# vim
 cp -v .vimrc ~/.vimrc
+vim +NeoBundleInstall +qall
+
+# zsh
 cp -v .zshrc ~/.zshrc
+
+# git
 cp -v .gitconfig ~/.gitconfig
 
